89d7fbf1baa8e962ca8e2bd3db90eda408d8bf86
Hylann <105493840+Hxlxnn@users.noreply.github.com>
2025-05-16 18:00:29 +0200

    Update Q1.R

--- Q1.R
+++ Q1.R
@@ -1,11 +1,1 @@
-library(dplyr)
-
-qualite <- read.table("qualite-de-lair-dans-le-reseau-de-transport-francilien (1).csv", header = TRUE, sep = ";", quote = "\"")
-
-
-qualite_clean <- qualite %>%
-  filter(rowSums(is.na(.)) != ncol(.)) %>%       
-  select(where(~ !all(is.na(.)))) %>%             
-  distinct()             
-View(qualite_clean)
 
