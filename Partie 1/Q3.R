f666e096015821a7a8e862a65ddb3bdccaa24ad3
abde-dev1937 <squalliabderrahim@gmail.com>
2025-05-21 22:58:19 +0200

    Update Q3.R

--- Partie 1/Q3.R
+++ Partie 1/Q3.R
@@ -1,7 +1,6 @@
 library(dplyr)
 
 qualite <- read.table("qualite-de-lair-dans-le-reseau-de-transport-francilien (1).csv", header = TRUE, sep = ";")
-head(qualite)
 
 qualite_clean <- qualite %>%
   filter(rowSums(is.na(.)) != ncol(.)) %>%       
@@ -9,24 +8,19 @@ qualite_clean <- qualite %>%
   distinct() 
 
 qualite_metro <- qualite_clean %>%
-  filter(grepl("Métro", Nom.de.la.ligne))  # On filtre les stations de métro
+  filter(grepl("Métro", Nom.de.la.ligne)) 
 
 
 View(qualite_metro)
 
-# Diviser le dataset en 70 % pour l'entraînement et 30 % pour le test
-set.seed(123)  # Pour reproduire l'échantillonnage aléatoire
+set.seed(123)  
 train_indices <- sample(1:nrow(qualite_metro), size = 0.7 * nrow(qualite_metro))
 
-# Créer le jeu d'entraînement (train) et le jeu de test (test)
 train_data <- qualite_metro[train_indices, ]
 test_data <- qualite_metro[-train_indices, ]
 
-# Sauvegarder les datasets dans des fichiers CSV
 write.csv(train_data, "train.csv", row.names = FALSE)
 write.csv(test_data, "test.csv", row.names = FALSE)
 
-#View(train_data)
-#View(test_data)
-
-
+View(train_data)
+View(test_data)
